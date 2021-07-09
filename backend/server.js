f908d664c70339440fa9c0cc06e672de2a458e48
sajjad.heidari <sajjadhaidari1994@gmail.com>
2021-07-09 12:25:47 +0430

    User auth endpoints

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,35 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import { notFound, errorHandler } from './middleware/errorMiddleware.js'
-import connectDB from './config/db.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
 
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.get('/', (req, res) => {
-  res.send('API is running....')
-})
+app.use(express.json());
 
-app.use('/api/products', productRoutes)
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 
-app.use(notFound)
-app.use(errorHandler)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000
+app.use(notFound);
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(
     `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
   )
-)
+);
